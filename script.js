9c0c4cecf9e2d8f6335fce9049e1547ef608df89
Sourav0809 <spathak7431@gmail.com>
2023-05-29 21:23:08 +0530

    ALL ADDED

--- script.js
+++ script.js
@@ -2,7 +2,25 @@
 const about = document.querySelector(".aboutme")
 const abouttag = document.querySelector(".about")
 abouttag.addEventListener("click", () => {
-    console.log("hello")
     const offsetTop = about.offsetTop
+    window.scrollTo(0, offsetTop)
+})
+
+
+const myskill = document.querySelector(".my-skills-section-main")
+document.querySelector(".myskill").addEventListener("click", () => {
+    const offsetTop = myskill.offsetTop
+    window.scrollTo(0, offsetTop)
+})
+const project = document.querySelector(".portfolio")
+document.querySelector(".project").addEventListener("click", () => {
+    const offsetTop = project.offsetTop
+    window.scrollTo(0, offsetTop)
+})
+
+
+const contactMe = document.querySelector(".scrool")
+document.querySelector(".contact-me").addEventListener("click", () => {
+    const offsetTop = contactMe.offsetTop
     window.scrollTo(0, offsetTop)
 })
